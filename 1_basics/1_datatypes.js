775d5758ee83498f411cae3219c6a6b1b6800dd0
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-05-19 14:19:55 +0000

    Datatypes

--- 1_basics/1_datatypes.js
+++ 1_basics/1_datatypes.js
@@ -1,3 +1,32 @@
 "use strict" 
 // treats all Js codes as newer versions
 
+// alert(2+2) 
+// We are using nodejs not browser . Yeh browser mein kaam karta 
+
+let name = "Swayam"
+let age = 20
+let isLoggedIn = true 
+let state = null
+
+// Primitive datatypes
+
+/*
+Number
+bigint
+String
+Boolean
+null => standalone value
+undefined => value has not been assigned
+symbol => unique
+*/
+
+// object
+
+console.log(typeof "Swayam") // String
+console.log(typeof age)// Number
+console.log(typeof null)//object
+console.log(typeof undefined) // undefined
+
+
+
