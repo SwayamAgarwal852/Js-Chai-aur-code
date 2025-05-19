0fe037ea9dedf685ea715016285bdd6343c6fa35
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-05-19 13:55:03 +0000

    change 1

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,19 @@
+const accountID = 203
+let accountEmail = "swayamfeb2005@gmail.com"
+var accountPass = "Swayamanushka*2"
+accountStatus = 1
+let accountCity 
+
+// accountID = 204
+
+accountEmail = "swayam@gmail.com"
+accountPass = "Swayam*2"
+accountStatus = 2
+
+// console.log(accountID);
+
+/*
+Prefer not to use var because of issue in block scope and fuctional scope
+*/
+
+console.table([accountID, accountEmail, accountPass, accountStatus, accountCity])
