d95181879994cd8a5e3136892d56c288f8a2e3e1
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-05-20 09:05:01 +0000

    conversion 2

--- 1_basics/1_conversion.js
+++ 1_basics/1_conversion.js
@@ -1,11 +1,11 @@
 let score = true
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let score2 = Number(score)
 
-console.log(typeof(score2))
-console.log(score2)
+// console.log(typeof(score2))
+// console.log(score2)
 
 // "33" => 33
 // "Swayam" => NaN
@@ -14,7 +14,42 @@ console.log(score2)
 
 let age = "Swayam"
 let Booleanvalue = Boolean(age)
-console.log(Booleanvalue)
+// console.log(Booleanvalue)
 
 // 1 => true and 0=> false
 // ""=> false and "Swayam" => true
+
+// Operations
+
+// console.log(3**4)
+
+let str1 = "Hello"
+let str2 = " Swayam"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.log(num1);
+// Not advisable for readability
+
+
+
+
+
+
+
+
+
+
+
+
+
