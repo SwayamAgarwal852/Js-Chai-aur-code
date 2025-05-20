9d4dd5815c912a076538a732d8bd480e4f990942
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-05-20 08:41:54 +0000

    conversion

---
+++ 1_basics/1_conversion.js
@@ -0,0 +1,20 @@
+let score = true
+console.log(typeof score)
+console.log(typeof(score))
+
+let score2 = Number(score)
+
+console.log(typeof(score2))
+console.log(score2)
+
+// "33" => 33
+// "Swayam" => NaN
+// "33abc" => NaN
+// true => 1 and false=> 0
+
+let age = "Swayam"
+let Booleanvalue = Boolean(age)
+console.log(Booleanvalue)
+
+// 1 => true and 0=> false
+// ""=> false and "Swayam" => true
