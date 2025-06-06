5280c93a4cee80f59e52e9669fa61aa24fc9465f
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-06-06 09:55:39 +0000

    array 2

--- 2_basics/2_array.js
+++ 2_basics/2_array.js
@@ -26,15 +26,45 @@ const myArray = [1,2,3,4,5]
 
 // slice and splice
 
-console.log("A", myArray)
+// console.log("A", myArray)
 
-const a1 = myArray.slice(1,3)
-console.log(a1)
-console.log("B", myArray)
+// const a1 = myArray.slice(1,3)
+// console.log(a1)
+// console.log("B", myArray)
 
-const a2 = myArray.splice(1,3)
-console.log(a2)
-console.log("3", myArray)
+// const a2 = myArray.splice(1,3)
+// console.log(a2)
+// console.log("3", myArray)
+
+const marvel = ["thor", "ironman", "hulk"]
+const dc = ["superman", "batman", "flash"]
+const indian = ["Krish", "Sethia"]
+
+// marvel.push(dc)
+// console.log(marvel)
+// console.log(marvel[3][0])
+
+// const heros = marvel.concat(dc)
+// console.log(heros)
+
+// const new_heros = [...indian, ...marvel, ...dc]
+// console.log(new_heros)
+
+// const another = [1,2,3,[4,5,6],7,[8,[9,0]]]
+// const normal_array = another.flat(Infinity)
+// console.log(normal_array)
+
+
+// console.log(Array.isArray("Swayam"))
+// console.log(Array.from("Swayam"))
+// console.log(Array.from({name:"Swayam"})) // intersting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
+
 
 
 
