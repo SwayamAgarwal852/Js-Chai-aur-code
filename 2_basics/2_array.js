c9bb2cedb9bbf8121200f9b5ae7fd3ebe89048b2
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-06-06 09:04:38 +0000

    array 1

---
+++ 2_basics/2_array.js
@@ -0,0 +1,41 @@
+// Array
+const myArray = [1,2,3,4,5]
+// const myArr = ["Swayam", "Krish", "Manoj"]
+// console.log(myArr)
+
+// const myArr2 = new Array(2,5,7,9)
+// console.log(myArr2)
+
+// Array methods
+
+// myArray.push(6)
+// myArray.pop()
+
+// myArray.unshift(0)
+// myArray.unshift(-1)
+// myArray.shift()
+// console.log(myArray)
+
+// console.log(myArray.includes(0))
+// console.log(myArray.indexOf(2));
+
+// const newArray = myArray.join()
+// console.log(myArray)
+// console.log(newArray)
+// console.log(typeof(newArray))
+
+// slice and splice
+
+console.log("A", myArray)
+
+const a1 = myArray.slice(1,3)
+console.log(a1)
+console.log("B", myArray)
+
+const a2 = myArray.splice(1,3)
+console.log(a2)
+console.log("3", myArray)
+
+
+
+
