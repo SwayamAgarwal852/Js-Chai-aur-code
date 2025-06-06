00e2a35ab676327026eabed47e61da134e9de5e3
SwayamAgarwal852 <swayamfeb2005@gmail.com>
2025-06-06 06:22:34 +0000

    Maths

---
+++ 1_basics/1_math.js
@@ -0,0 +1,36 @@
+// Numbers
+
+const score = new Number(200)
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(1))
+
+const balance = 26.78937
+// console.log(balance.toPrecision(2));
+
+// const hundreds = 10000000000
+// console.log(hundreds.toLocaleString());
+
+// maths
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(3.2))
+// // console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(3,4,5,6))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+
+// Important formula
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
